10f24daba13d1426167379b8cf46d22d3162b592
Marcus Almeida <mpereirassa@gmail.com>
2026-07-22 12:18:47 -0300

    feat: update version to 0.5.0 and implement database introspection support

--- flask_mvc/flask_mvc.py
+++ flask_mvc/flask_mvc.py
@@ -1,26 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from method_override.wsgi_method_override import MethodOverrideMiddleware
 
 from . import cli
 from .middlewares.html_input_method_helper import HTMLInputMethodHelper
 from .middlewares.blueprint_binding import BlueprintBinding
 from .middlewares.router import Router
-
+from .middlewares.base_model import FlaskReflection
 
 class FlaskMVC:
-    def __init__(self, app: Flask = None, path="app"):
+    def __init__(self, app: Flask = None, path="app", db: SQLAlchemy = None):
         if app is not None:
-            self.init_app(app, path)
+            self.init_app(app, path, db)
 
-    def init_app(self, app: Flask = None, path="app"):
-        self.perform(app, path)
+    def init_app(self, app: Flask = None, path="app", db: SQLAlchemy = None):
+        self.perform(app, path, db)
 
-    def perform(self, app: Flask, path: str):
+    def perform(self, app: Flask, path: str, db: SQLAlchemy = None):
         self._configure_template_folder(app)
         self._configure_method_override_middleware(app)
         self._configure_blueprint_middleware(app, path)
         self._inject_object_in_jinja_template(app)
         self._configure_cli_commands(app)
+        self._configure_instrospection_database(app, db)
 
     def _configure_template_folder(self, app):
         app.template_folder = "views"
@@ -30,6 +32,9 @@ class FlaskMVC:
 
     def _configure_blueprint_middleware(self, app, path):
         BlueprintBinding(app, path).register()
+
+    def _configure_instrospection_database(self, app, db):
+        FlaskReflection(app, db)
 
     def _inject_object_in_jinja_template(self, app):
         @app.context_processor
--- tests/app/models/message.py
+++ tests/app/models/message.py
@@ -1,8 +1,15 @@
+from sqlalchemy import Column, Integer, String, Table
+
+from flask_mvc.middlewares.base_model import ReflectedModel
 from tests.app import db
 
+messages_table = Table(
+    "messages",
+    db.metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("title", String(100), nullable=False),
+)
 
-class Message(db.Model):
+
+class Message(ReflectedModel, db.Model):
     __tablename__ = "messages"
-
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
